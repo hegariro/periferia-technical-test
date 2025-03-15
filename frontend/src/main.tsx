1787909427909a18527097693504f2824707ec25
Heiner Ríos Rodríguez <heinerr.rodriguez@gmail.com>
2025-03-15 14:47:31 -0500

    fix: branch conflict

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { store } from './app/store'
+import { store } from './app/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
